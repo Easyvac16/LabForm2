015d5552f9b25c8c8e38d5d628ac0e7a83ef9324
Easyvac16 <gdima9051@gmail.com>
2023-06-22 17:57:58 +0300

    Add project files.

---
+++ LabForm2/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace LabForm2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            Point_X = new Label();
+            Point_Y = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(90, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Mouse";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 54);
+            label2.Name = "label2";
+            label2.Size = new Size(17, 15);
+            label2.TabIndex = 1;
+            label2.Text = "X:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 92);
+            label3.Name = "label3";
+            label3.Size = new Size(17, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Y:";
+            // 
+            // Point_X
+            // 
+            Point_X.AutoSize = true;
+            Point_X.Location = new Point(32, 54);
+            Point_X.Name = "Point_X";
+            Point_X.Size = new Size(0, 15);
+            Point_X.TabIndex = 3;
+            // 
+            // Point_Y
+            // 
+            Point_Y.AutoSize = true;
+            Point_Y.Location = new Point(32, 92);
+            Point_Y.Name = "Point_Y";
+            Point_Y.Size = new Size(0, 15);
+            Point_Y.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonFace;
+            ClientSize = new Size(542, 403);
+            Controls.Add(Point_Y);
+            Controls.Add(Point_X);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            ForeColor = SystemColors.ActiveCaptionText;
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            MouseClick += Form1_MouseClick;
+            MouseDown += Form1_MouseDown;
+            MouseMove += Form1_MouseMove;
+            MouseUp += Form1_MouseUp;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label Point_X;
+        private Label Point_Y;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+    }
+}
---
+++ LabForm2/Form1.cs
@@ -0,0 +1,78 @@
+﻿namespace LabForm2
+{
+    public partial class Form1 : Form
+    {
+        bool Down;
+        Point startPoint;
+        List<Label> labels = new List<Label>();
+        char currentSybmol;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point_X.Text = e.X.ToString();
+            Point_Y.Text = e.Y.ToString();
+
+            if (e.Button == MouseButtons.Left)
+            {
+                currentSybmol = '*';
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                currentSybmol = '^';
+            }
+            else if (e.Button == MouseButtons.Middle)
+            {
+                currentSybmol = '♩';
+            }
+
+
+            if (Down)
+            {
+                Label label = new Label();
+                label.AutoSize = true;
+                label.Location = new Point(e.X, e.Y);
+                label.Text = currentSybmol.ToString();
+                this.Controls.Add(label);
+                labels.Add(label);
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Down = true;
+            startPoint = e.Location;
+
+            Label label = new Label();
+            label.AutoSize = true;
+            label.Location = startPoint;
+            label.Text = currentSybmol.ToString();
+            this.Controls.Add(label);
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) Down = false;
+            else if (e.Button == MouseButtons.Right) Down = false;
+            else if (e.Button == MouseButtons.Middle) Down = false;
+        }
+    }
+}
